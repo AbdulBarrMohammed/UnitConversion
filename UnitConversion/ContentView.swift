28e6b4889883b2b84644c3d7c71887ed97ad71ba
Abdul-Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-08-16 15:02:54 -0400

    started layout

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = 0.0
+    @State private var outputValue = 0.0
+    @State private var inputUnitPicked = "meters"
+    @State private var outputUnitPicked = "yard"
+    
+    let inputUnits = ["meters", "kilometers", "feet", "yard", "miles"]
+    let outputUnits = ["meters", "kilometers", "feet", "yard", "miles"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            Form {
+                Section(header:Text("Enter starting length value and units")){
+                    TextField("Unit input", value: $inputValue, format: .number)
+                    Picker("Choose units", selection: $inputUnitPicked) {
+                        ForEach(inputUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section(header:Text("Choose conversion units")) {
+                    Picker("Choose units", selection: $outputUnitPicked) {
+                        ForEach(outputUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section(header:Text("Conversion results")) {
+                    TextField("Results", value: $outputValue, format: .number)
+                }
+            }
+            .navigationTitle("Unit Conversion")
         }
-        .padding()
+        
+        
     }
 }
 
