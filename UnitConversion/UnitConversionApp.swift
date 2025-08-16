d1f006d16abc92a28ad3ed7d2c24dc091c9628c1
Abdul-Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-08-16 13:56:51 -0400

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Barr Mohammed on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitConversionTests/UnitConversionTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionTests.swift
+//  UnitConversionTests
+//
+//  Created by Barr Mohammed on 8/16/25.
+//
+
+import Testing
+@testable import UnitConversion
+
+struct UnitConversionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
